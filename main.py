8808f484d086e8ca9e4b35aa61ec98de08b159f6
laowangbaba <976609856@qq.com>
2021-07-09 13:08:40 +0800

    main test

---
+++ main.py
@@ -0,0 +1,23 @@
+# Script Name   : folder_size.py
+# Author        : Craig Richards
+# Created       : 19th July 2012
+# Last Modified	: 22 February 2016
+# Version       : 1.0.1
+
+# Modifications : Modified the Printing method and added a few comments
+
+# Description   : This will scan the current directory and all subdirectories and display the size.
+
+import os
+import sys  # Load the library module and the sys module for the argument vector'''
+
+try:
+    directory = sys.argv[1]  # Set the variable directory to be the argument supplied by user.
+except IndexError:
+    sys.exit("Must provide an argument.")
+
+dir_size = 0  # Set the size to 0
+fsizedicr = {'Bytes': 1,
+             'Kilobytes': float(1) / 1024,
+             'Megabytes': float(1) / (1024 * 1024),
+             'Gigabytes': float(1) / (1024 * 1024 * 1024)}
