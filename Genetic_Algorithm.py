fbb2f4e57bcce33f1631b1c6961e65c3690fbe05
laowangbaba <976609856@qq.com>
2021-07-09 13:43:53 +0800

    上上传文件

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -1,16 +1,28 @@
 #遗传算法求最小值
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import random
+
+def fitness_func(X):
+	a=10
+	pi=np.pi
+	x=X[:,0]
+	y=X[:,1]
+	return 2*a+x**2+y**2-a*np.cos(2*pi*x)-a*np.cos(2*pi*y)
+
+def decode(x,a,b):
+	xt=0
+	for i in range(len(x)):
+		xt=xt+x[i]*np.power(2,i)
+	return a+xt*(b-a)/(np.power(2,len(x))-1)
+
+def decode_X(X:np.array):
 
 
 
 
 
-
-
-
-
-def ga():
+ ga():
 	"""变异操作"""
 	c=0.3
 	m=0.05
