5efae51b858f93410a5770c589a571fda9489549
laowangbaba <976609856@qq.com>
2021-07-09 17:11:30 +0800

    2021/07/09 17:11

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -18,16 +18,16 @@ def decode(x,a,b):
 	return a+xt*(b-a)/(np.power(2,len(x))-1)
 
 def decode_X(X:np.array):
-	X2=np.zeros(X.shape[0],2)
+	X2=np.zeros((X.shape[0],2))
 	for i in range(X.shape[0]):
-		x[i]=decode(X[i,:20],-5,5)
-		y[i]=decode(X[i,20:],-5,5)
-		X2[i,:]=np.array(x[i],y[i])
+		xi=decode(X[i,:20],-5,5)
+		yi=decode(X[i,20:],-5,5)
+		X2[i,:]=np.array(xi,yi)
 	return X2
 
 def select(X,fitness):   #根据概率得到X的不同行
 	fitness=1/fitness
-	fitnesss=fitness/fitness.sum()
+	fitness=fitness/fitness.sum()
 	idx=np.array(list(range(X.shape[0])))
 	X2_inx=np.random.choice(idx,X.shape[0],p=fitness)
 	X2=X[X2_inx,:]
@@ -56,10 +56,10 @@ def ga():
 	"""变异操作"""
 	c=0.3
 	m=0.05
-	best_finess=[]
+	best_fitness=[]
 	best_xy=[]
 	best_num=[]
-	iter_num=[]
+	iter_num=100
 	X0=np.random.randint(0,2,(50,40)) #0,1矩阵
 	for i in range(iter_num):
 		X1=decode_X(X0)
@@ -70,12 +70,14 @@ def ga():
 
 		X5=decode_X(X4)
 		fitness=fitness_func(X5)
-		best_finess.append(fitness.min())
+		best_fitness.append(fitness.min())
 		x,y=X5[fitness.argmin()]
 		best_xy.append((x,y))
 		X0=X4
 	
 	print("最优解是：%.5f" % best_fitness[-1])
-	return 0
+	print("最优解是：x=%.5f,y=%.5f" % best_xy[-1])
+	plt.plot(best_fitness,color='r')
+	plt.show()
 
 ga()
