319ec57c9b746eff29e1999659e07c7b07eb7e75
laowangbaba <976609856@qq.com>
2021-07-09 13:26:04 +0800

    Genetic

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -2,6 +2,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+
+
+
+
+
+
 def ga():
 	"""变异操作"""
-	for i
+	c=0.3
+	m=0.05
+	best_finess=[]
+	best_xy=[]
+	best_num=[]
+	iter_num=[]
+	x0=np.random.randint(0,2,(50,40)) #0,1矩阵
+	for i in range(iter_num):
+		X1=decode_X(X0)
+		fitness =fitness_func(X1)
